0f5755c125e1595ba0a9cf108221442767b8eb34
Josimar16 <josimarjr479@gmail.com>
2021-03-23 21:45:16 -0300

    refactor: :recycle: Retirando um pouco da regra de negocio
    Retirando um pouco da regra de negocio dos routes e colocando no model

--- backend/src/routes/categories.routes.ts
+++ backend/src/routes/categories.routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+
 import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
@@ -9,16 +9,16 @@ const categories: Category[] = [];
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const category: Category = {
-    id: uuidV4(),
+  const category = new Category();
+  Object.assign(category, {
     name,
     description,
     created_at: new Date()
-  }
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json(category);
 });
 
 export { categoriesRoutes };
