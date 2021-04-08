314a8b5471433a562d7934b5ee65eda07922ce2a
Josimar16 <josimarjr479@gmail.com>
2021-04-08 12:33:47 -0300

    refactor: :recycle: Resolvendo bugs nos testes

--- backend/src/modules/accounts/infra/http/routes/users.routes.ts
+++ backend/src/modules/accounts/infra/http/routes/users.routes.ts
@@ -4,7 +4,7 @@ import multer from 'multer';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/updateUserAvatarController';
 
-import uploadConfig from '@modules/accounts/../../config/upload';
+import uploadConfig from '@config/upload';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
