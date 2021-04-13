55e810a573f780070b5e61dd9a171ad94c242e8c
Josimar16 <josimarjr479@gmail.com>
2021-04-13 20:57:54 -0300

    perf: :sparkles: Implementando middleware
    Implementando middleware de autenticação e se é admin na rota de criar carro

--- backend/src/modules/cars/infra/http/routes/cars.routes.ts
+++ backend/src/modules/cars/infra/http/routes/cars.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 
 const carsRouter = Router();
 const createCarController = new CreateCarController();
 
-carsRouter.post('/', createCarController.handle);
+carsRouter.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 
 export { carsRouter }
