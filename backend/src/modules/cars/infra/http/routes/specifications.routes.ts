5aabb1953829dc96b38bc25a0ca7904732c1a92c
Josimar16 <josimarjr479@gmail.com>
2021-04-13 21:05:20 -0300

    feat: :zap: Implementando autenticação nas rotas
    Implementando autenticação nas rotas de categorias e especificações

--- backend/src/modules/cars/infra/http/routes/specifications.routes.ts
+++ backend/src/modules/cars/infra/http/routes/specifications.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecifications/CreateSpecificationController';
 import { ListSpecificationsController } from '@modules/cars/useCases/listSpecifications/ListSpecificationsController';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post('/', ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
 
 specificationsRoutes.get('/', listSpecificationsController.handle)
 
